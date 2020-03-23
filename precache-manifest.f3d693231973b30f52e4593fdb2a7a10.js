4277d0c16d73bdb3a5ecf215939279299dd520a9
Peter <peterfink89@yahoo.com>
2020-03-23 14:26:01 -0700

    Updates

---
+++ precache-manifest.f3d693231973b30f52e4593fdb2a7a10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "374c314170092d39ab8ec1f32824fd6f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "42fe56910f460b924733",
+    "url": "/monsters-rolodex/static/css/main.e42a316b.chunk.css"
+  },
+  {
+    "revision": "4ecb9f0df0474c1b1fa9",
+    "url": "/monsters-rolodex/static/js/2.47062208.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.47062208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42fe56910f460b924733",
+    "url": "/monsters-rolodex/static/js/main.e5aa4dc8.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
